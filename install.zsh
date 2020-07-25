707259eae911de44d632413a9a4001ddbdd6695e
Mick G <68760212+Token-Thinker@users.noreply.github.com>
2020-07-24 22:43:12 -0400

    Create install.zsh

---
+++ install.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/tklab
+# And also installs Homebrew Packages
+# And sets Sublime preferences
+############################
+
+echo "Hello World!"

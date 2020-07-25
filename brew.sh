e7fe04f9fa00d14f9073324a84c87d873170f564
Mick G <68760212+Token-Thinker@users.noreply.github.com>
2020-07-25 03:37:32 -0400

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+#brew cask install google-chrome
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install ultimaker-cura
+brew cask install autodesk-fusion360
+brew cask install kicad

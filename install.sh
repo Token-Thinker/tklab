215a3f6643e6f501f842c450da932fcacd4fad5f
Mick G <68760212+Token-Thinker@users.noreply.github.com>
2020-07-25 02:19:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,35 @@
 # And sets Sublime preferences
 ############################
 
-echo "Hello World!"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+# dotfiles directory
+tklabdir=${homedir}/tklab
+
+# list of files/folders to symlink in ${homedir}
+files="bash_profile bashrc bash_prompt aliases private"
+
+# change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# create symlinks (will overwrite old dotfiles)
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+done
+
+# Download Git Auto-Completion
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh" > ${homedir}/.git-completion.zsh
+
+# Run the Homebrew Script
+./brew.sh
+
+# Run the Sublime Script
+./sublime.sh
